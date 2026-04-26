653a5e79e9323d82c5bcebecdc99ff18211d8d8f
彼岸临窗 <1895@88.com>
2026-04-27 00:02:42 +0800

    更新官网域名（部分更新）

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 /**
  *
  * 一款简约文艺的文字博客主题：那些物质的东西，都会随着时间慢慢销蚀，而我们写下的文字，最趋近于永恒。唯愿不忘初心，坚持把自己的博客写下去。本主题作者是一名律师，工作越来越忙，主题事宜请先仔细查阅官方文档，也可在QQ交流群939170079咨询博友。
- * 官网：<a href="https://oneblog.net">oneblog.net</a>
+ * 官网：<a href="https://onenote.io">onenote.io</a>
  * 文档：<a href="https://docs.oneblog.net">docs.oneblog.net</a>
  * 
  * @package OneBlog
  * @author 彼岸临窗
  * @version 3.6.5
- * @link https://oneblog.net
+ * @link https://onenote.io
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
